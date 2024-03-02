299ee0aa268ee7f9d640201c16e1d18d34910c81
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-03-02 19:20:52 -0300

    standard comparator

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,26 @@
+package org.example.application;
+
+import org.example.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 350.50));
+
+        Collections.sort(list);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
