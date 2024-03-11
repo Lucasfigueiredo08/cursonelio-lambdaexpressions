6d5345364384116d2b36c99800f94b4d739fb1ae
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-03-11 18:15:09 -0300

    Implementação da interface

---
+++ src/main/java/org/example/consumer/util/PriceUpdate.java
@@ -0,0 +1,12 @@
+package org.example.consumer.util;
+
+import org.example.consumer.entitites.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+    @Override
+    public void accept(Product product) {
+        product.setPrice(product.getPrice() * 1.1);
+    }
+}
