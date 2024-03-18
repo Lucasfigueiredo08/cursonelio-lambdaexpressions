81448d22df0ea400d7166a8023873b1f16a88a5d
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-03-18 19:16:19 -0300

    Implementando interface function<A, B>

---
+++ src/main/java/org/example/functions/utils/UpperCaseName.java
@@ -0,0 +1,12 @@
+package org.example.functions.utils;
+
+import org.example.functions.entities.Product;
+
+import java.util.function.Function;
+
+public class UpperCaseName implements Function<Product, String> {
+    @Override
+    public String apply(Product product) {
+        return product.getName().toUpperCase();
+    }
+}
